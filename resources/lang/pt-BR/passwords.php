5a56dea712585c6c340a97c9413e12f1615edc21
Ismael Vaz <ismael.hvaz@gmail.com>
2025-05-27 21:39:00 -0300

    feat: adicionado pagina de tradução para pt-br

---
+++ resources/lang/pt-BR/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => 'Senha foi redefinida.',
+    'sent' => 'Link de redefinição de senha enviado por e-mail.',
+    'throttled' => 'Aguarde antes de tentar novamente.',
+    'token' => 'Token de redefinição de senha inválido.',
+    'user' => "Não foi encontrado um usuário com esse endereço de e-mail.",
+
+];
