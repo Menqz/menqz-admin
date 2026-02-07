b417109eec866e11118de3fd6b14ac1fd9f855dc
Ismael Vaz <ismael.hvaz@gmail.com>
2026-02-07 12:43:31 -0300

    fix: ajusta controle de hooks no parts controller

--- src/Controllers/AdminPartController.php
+++ src/Controllers/AdminPartController.php
@@ -2,13 +2,17 @@
 
 namespace MenqzAdmin\Admin\Controllers;
 
+use Illuminate\Routing\Controller;
 use Illuminate\Database\Eloquent\Model;
 use MenqzAdmin\Admin\Grid;
 use MenqzAdmin\Admin\Form;
 use Illuminate\Http\Request;
+use MenqzAdmin\Admin\Traits\HasCustomHooks;
 
-abstract class AdminPartController
+abstract class AdminPartController extends Controller
 {
+
+    use HasCustomHooks;
     /**
      * The parent model instance.
      *
@@ -29,6 +33,21 @@ abstract class AdminPartController
     public function __construct(?Model $parentModel = null)
     {
         $this->parentModel = $parentModel;
+
+        $this->hook("alterGrid", function ($scope, $grid) {
+            // alter the grid here
+            return $grid;
+        });
+
+        $this->hook("alterDetail", function ($scope, $detail) {
+            // alter the detail here
+            return $detail;
+        });
+
+        $this->hook("alterForm", function ($scope, $form) {
+            // alter the form here
+            return $form;
+        });
     }
 
     /**
@@ -45,6 +64,34 @@ abstract class AdminPartController
         }
 
         return $this->title ?: class_basename(static::class);
+    }
+
+    public function index()
+    {
+        $grid = $this->grid();
+        if ($this->hasHooks('alterGrid')) {
+            $grid = $this->callHooks('alterGrid', $grid);
+        }
+
+        return $grid->render();
+    }
+
+    public function create()
+    {
+        $form = $this->form();
+        if ($this->hasHooks('alterForm')) {
+            $form = $this->callHooks('alterForm', $form);
+        }
+        return $form->render();
+    }
+
+    public function edit()
+    {
+        $form = $this->form();
+        if ($this->hasHooks('alterForm')) {
+            $form = $this->callHooks('alterForm', $form);
+        }
+        return $form->render();
     }
 
     /**
@@ -69,10 +116,12 @@ abstract class AdminPartController
      */
     public function handle(Request $request)
     {
-        if ($request->get('_mode') === 'form') {
-            return $this->form()->render();
+        if ($request->get('_mode') === 'create') {
+            return $this->create();
+        } else if ($request->get('_mode') === 'edit') {
+            return $this->edit();
         }
 
-        return $this->grid()->render();
+        return $this->index();
     }
 }
