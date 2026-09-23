0513f4d7b35a659ee5424c9584640d0860823ae8
Ismael Vaz <ismael.hvaz@gmail.com>
2026-09-22 22:31:46 -0300

    fix: ajusta container do choice no filtro (#34)
    - ajustado container do choice no filtro

--- src/Grid/Filter/Presenter/Select.php
+++ src/Grid/Filter/Presenter/Select.php
@@ -94,7 +94,10 @@ class Select extends Presenter
             'removeItemButton'   => true,
             'allowHTML'          => true,
             'classNames'         => [
-                'containerOuter' => 'choices '.$this->getElementClass(),
+                'containerOuter' => [
+                    'choices',
+                    $this->getElementClass()
+                ],
             ],
         ], $this->config);
         $configs = json_encode($configs);
